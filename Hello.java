c40f619bcf619fbc1cbecbe836adebad6ce06f8e
smuniza1 <sidmuniza@gmail.com>
2021-05-26 17:20:26 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+/* Java Program Name
+* Muniza Siddiqui
+* May 26, 2021
+* CMSC 255 and C01 */
+
+public class Hello {
+	public static void main (String [] args) {
+	System.out.println("<Muniza Siddiqui>");
+	System.out.println("<Hello>");
+	System.out.println("<Lab 1>");
+	System.out.println("<May 26, 2021>");
+	System.out.println("<CMSC 255 and C01>");
+	System.out.println();
+	}
+}
